3315a2b141655a43552d506ed83d1df895928b54
ishaan83s <berealishaan@gmail.com>
2026-08-18 21:26:11 +0530

    Updated default origins of localhost

--- ps25/backend/main.py
+++ ps25/backend/main.py
@@ -51,6 +51,8 @@ allowed_origins = [orig.strip() for orig in cors_origins_env.split(",") if orig.
 default_origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
